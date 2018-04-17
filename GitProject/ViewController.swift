f9c8fbed30b620073c6196915ad3d13e4b60e531
Santhosh <santhosh-mac@santhoshs-mac.local>
2018-04-17 12:00:24 +0530

    check

--- GitProject/ViewController.swift
+++ GitProject/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var setTitle: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    print("this is first github project")
     }
 
     override func didReceiveMemoryWarning() {
